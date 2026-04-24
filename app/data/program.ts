29b10070714094a5262b7b621e67b880d116093f
denandras <d.enandras@gmail.com>
2026-04-24 10:20:01 +0200

    Add Botos Gergely Kvintett description (Regina request)

--- app/data/program.ts
+++ app/data/program.ts
@@ -4,15 +4,20 @@ export interface Piece {
   title: string;
   performers: string[];
   description: string;
+  poem?: string;
+  poemYear?: string;
+  poemAuthor?: string;
+  poemTranslator?: string;
 }
 
 export const concertInfo = {
-  title: "Alkalmazott zeneszerzés BA diplomakoncert",
+  title: `Zeneszerzés és Alkalmazott Zeneszerzés
+BA diplomakoncert`,
   date: "2026. május 3.",
   time: "19:00",
-  venue: "Liszt Ferenc\nZeneművészeti Egyetem",
+  venue: "Zeneakadémia, Solti terem",
   venueEn: "Liszt Academy Budapest",
-  note: "",
+  note: "A koncert előtt, 16.00 órától a diplomázók vizsgafilmjei az I. előadó teremben tekinthetők meg.",
 };
 
 export const program: Piece[] = [
@@ -25,24 +30,49 @@ export const program: Piece[] = [
       "Mészáros Jázon Márk (klarinét)",
       "Csongár Márton (basszetkürt)",
       "Lakatos Béla (basszusklarinét)",
-      "Botos Gergely (kontrabasszus-klarinét)"
+      "Botos Gergely (kontrabasszus-klarinét)",
     ],
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+    description: "Ebben a darabomban igyekeztem a klarinét hangszercsalád sokszínűségét bemutatni a legelterjedtebb szoprántól kezdve egészen a ritkán használt kontrabasszus-klarinétig.",
   },
   {
     id: 2,
     composer: "Sepsi Botond",
     title: "Egyenes labirintus",
-    performers: [
-      "Törőcsik Kristóf: A lágy fényhez fohászkodom",
-      "Végh Janka (szoprán)",
-      "Vajda Denejra (hárfa)",
-      "Friderikusz Péter (cselló)"
-    ],
-    description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis."
+    performers: [],
+    description: "",
   },
   {
     id: 3,
+    composer: "Törőcsik Kristóf",
+    title: "A lágy fényhez fohászkodom",
+    performers: [
+      "Végh Janka (szoprán)",
+      "Vajda Denejra (hárfa)",
+      "Friderikusz Péter (cselló)",
+    ],
+    description: "Anna Ahmatova XX. századi orosz költő versének megzenésítése Konczek József fordításában. A szopránra, hárfára és csellóra íródott darab egyfajta kontemplatív érzelmi kettősséget próbál megeleveníteni.",
+    poem: `A lágy fényhez fohászkodom…
+(„Moljusz okonnomu lucsu…”)
+
+A lágy fényhez fohászkodom,
+Szitál az ablakon.
+S egész nap nincs mit mondanom,
+A szív - dereng-borong.
+Kis fémlavórom peremén,
+Hol zöldre vált a réz,
+Úgy játszadoz a könnyű fény,
+Vidáman elbecéz.
+Ily egyszerű és bűntelen
+A csendes alkonyon
+E profán szentély szűntelen,
+S miként aranynak ünnepe,
+Oly hű vigasztalóm.`,
+    poemYear: "1909",
+    poemAuthor: "Anna Ahmatova",
+    poemTranslator: "Fordította: Konczek József",
+  },
+  {
+    id: 4,
     composer: "Nagy Emma",
     title: "Metallic Flowers",
     performers: [
@@ -50,68 +80,98 @@ export const program: Piece[] = [
       "Szűcs Boglárka (hegedű)",
       "Lachegyi Róza (brácsa)",
       "Bali Gabriella (cselló)",
-      "Szatzker Zsanett (harmonika)"
+      "Szatzker Zsanett (harmonika)",
     ],
-    description: "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet."
+    description: "",
   },
   {
-    id: 4,
-    composer: "Sebestyén Lázár Regina",
+    id: 5,
+    composer: "Sebestyén-Lázár Regina",
     title: "something, someday, somewhere...",
     performers: [
-      "Réz körút",
+      "Réz körút:",
       "Nagy Sándor (trombita)",
       "Szilágyi Dusán (trombita)",
       "Huszti Boldizsár (trombita)",
       "Faragó István (kürt)",
       "Gulyás Buda (harsona)",
       "Dénes András (harsona)",
-      "Vida Mátyás (tuba)"
+      "Vida Mátyás (tuba)",
     ],
-    description: "Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    description: "",
   },
   {
-    id: 5,
+    id: 6,
     composer: "Varga Nadin",
     title: "Glimmer",
-    performers: [
-      "Kalafszky Adriána (szoprán)",
-      "Szabó Dénes (cselló)"
-    ],
-    description: "Ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident."
+    performers: ["Kalafszky Adriána (szoprán)", "Szabó Dénes (cselló)"],
+    description: "A versben szereplő szavak egymás után olvasva nem alkotnak összefüggő jelentést, ugyanakkor hangzásuk és önálló jelentésük különféle benyomásokat keltenek. A lágyabb, zeneibb hangzású szavak inkább a kellemes érzetet erősítik, míg a keményebb hangzásúak feszültséget teremtenek – bár mindez természetesen teljesen szubjektív. Számomra a szöveg egy estétől hajnalig tartó folyamat képzetét idézte fel, amelyet a darabban is igyekeztem megjeleníteni.",
+    poem: `mist thin sheer dim bleak
+mist thin still drift green sheen
+deep sleep dream breeze silent silver
+evening melody horizon slowly
+over drowsy low lone soft small
+fall slow low lone
+soft small fall
+s
+o
+m
+n
+a
+m
+b
+u
+l
+i
+s
+m
+calm balm palm warm old soul
+gloom bloom gloam loom loam room moon
+moor mourn storm droop croon swoon drawl
+hollow sorrow follow borrow morrow shadow
+lonesome soothing moody autumn snowfall dawn dusk
+moon room loom gloom bloom gloam loam calm balm palm warm
+fog pond swarm spawn torn drawn drowse drone groan lull slow roam coast yore
+dream deep sleep dream drift drift green sheen
+mist thin sheer still drift sleep dream
+green sheen mist thin sheer
+mist thin
+mist`,
+    poemYear: "2026",
+    poemAuthor: "Zsigmond Soma",
   },
   {
     id: -1,
     composer: "",
     title: "SZÜNET",
     performers: [],
-    description: ""
+    description: "",
   },
   {
-    id: 6,
+    id: 7,
     composer: "Botos Gergely",
     title: "Hajnaltól éjfélig",
     performers: [
       "Kerti Júlia (mezzoszoprán)",
       "Németh Flóra (fagott/kontrafagott)",
-      "Botos Gergely (zongora)"
+      "Botos Gergely (zongora)",
     ],
-    description: "Sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium."
+    description: "",
   },
   {
-    id: 7,
+    id: 8,
     composer: "Nagy Emma",
-    title: "Limbs Move Better/Satellite",
+    title: "Limbs Move Better / Satellite",
     performers: [
       "Harcsa Veronika (ének)",
       "Horváth Áron (cimbalom)",
       "Gyányi Marcell (nagybőgő)",
-      "Kapolcsi-Szabó Levente (zongora)"
+      "Kapolcsi-Szabó Levente (zongora)",
     ],
-    description: "Totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur."
+    description: "",
   },
   {
-    id: 8,
+    id: 9,
     composer: "Varga Nadin",
     title: "Blue Jungle",
     performers: [
@@ -119,49 +179,91 @@ export const program: Piece[] = [
       "Botos Gergely (basszusklarinét)",
       "Pintér Anna (fuvola)",
       "Holozsai Eszter (fuvola)",
-      "Sebestyén Lázár Kata (fuvola)",
-      "Szilágyi Péter (marimba)"
+      "Sebestyén-Lázár Kata (fuvola)",
+      "Szilágyi Péter (marimba)",
     ],
-    description: "Aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet."
+    description: "Ez a darab az esőerdők gazdag és sokszínű hangzásvilágának emlékére született. A különféle hangszeres effektusok ennek a különleges természeti környezetnek az atmoszféráját idézik meg: a csepegő eső hangját, a szél susogását, valamint a különböző madárfajok énekét. Mivel az esőerdők napjainkban egyre nagyobb veszélynek vannak kitéve, a mű egyfajta tisztelgés is e még létező, különleges erdei hangzásvilág előtt.",
   },
   {
-    id: 9,
+    id: 10,
     composer: "Sepsi Botond",
     title: "Reframe",
+    performers: [],
+    description: "",
+  },
+  {
+    id: 11,
+    composer: "Törőcsik Kristóf",
+    title: "Nitzakhon",
     performers: [
-      "Törőcsik Kristóf: Nitzakhon",
       "Krulik Eszter (hegedű)",
       "Szűcs Boglárka (hegedű)",
       "Lachegyi Róza (brácsa)",
       "Koppán Kata (brácsa)",
-      "Bali Gabriella (cselló)"
+      "Bali Gabriella (cselló)",
     ],
-    description: "Consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam."
+    description: "Egy lassan kibomló vonósdarab, amely finom texturális változásokon és fokozatos dinamikai építkezésen keresztül jut el egy intenzív tetőpontig, majd visszahullik a kezdeti lebegő csendbe.",
   },
   {
-    id: 10,
+    id: 12,
     composer: "Botos Gergely",
     title: "Mémoire",
-    performers: [
-      "Erdő Zoltán (tárogató)",
-      "Botos Gergely (zongora)"
-    ],
-    description: "Quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate."
+    performers: ["Erdő Zoltán (tárogató)", "Botos Gergely (zongora)"],
+    description: "Ezzel a darabommal igyekeztem megidézni a századforduló francia zenei világát, ami a tárogató különleges hangszínével ötvözve egy kellemes hangulatot hoz magával. A koncerten ez a darab egy eredeti Stowasser tárogatón szólal meg.",
   },
   {
-    id: 11,
-    composer: "Sebestyén Lázár Regina",
+    id: 13,
+    composer: "Sebestyén-Lázár Regina",
     title: "Detachment",
     performers: [
       "Csörgeő Luca (ének)",
       "Szikora Adél (ének)",
       "Vámos Emma (ének)",
       "Farkas Botond (zongora)",
-      "Szalovszky Viktória (preparáció)"
+      "Szalovszky Viktória (preparáció)",
     ],
-    description: "Vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque."
-  }
+    description: "",
+    poem: `Elegy
+through scattered lights
+can be seen the One
+thrown by time
+on shores
+of the sea
+go blind
+where it founds
+the meeting what's complete
+In parting
+
+Moments
+You called to Yourself
+two apple
+a cold sandwich
+and vocal cord bouquet
+if You tied my voice
+as first sunburn after the winter
+on shores like icebergs
+my face is scattered by shards on my sole
+You know maybe I never loved You
+while wondering
+I feed Myself with Your kisses
+which We thought were true
+so much that separation seemed as dusting
+
+Detachment
+prisoned by Myself I'm counting the days
+like a dog without it's owner
+a captive moment
+faraway notes
+it speaks for itself
+if You do not come
+and Your face becomes memory
+on strangers faces
+forgotten by the wind
+like a spider web flowing in the breeze`,
+    poemYear: "2025",
+    poemAuthor: "Zselyke Szentgyörgyi",
+  },
 ];
 
-export const getPieces = () => program.filter(p => p.id > 0);
-export const getPieceById = (id: number) => program.find(p => p.id === id);
+export const getPieces = () => program.filter((p) => p.id > 0);
+export const getPieceById = (id: number) => program.find((p) => p.id === id);
