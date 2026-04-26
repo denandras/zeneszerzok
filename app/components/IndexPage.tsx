9a931b0ddce37d123fba17afd72df736e26a37c1
denandras <d.enandras@gmail.com>
2026-04-26 15:07:05 +0200

    Reduce to one empty line before first piece on programme page

--- app/components/IndexPage.tsx
+++ app/components/IndexPage.tsx
@@ -38,8 +38,7 @@ export default function IndexPage({ onSelectPiece }: IndexPageProps) {
           {/* Header height spacer - reduced */}
           <div className="h-16 md:h-24 flex-shrink-0" />
           
-          {/* Two empty lines before first piece */}
-          <div className="h-4" />
+          {/* Empty line before first piece */}
           <div className="h-4" />
           
           {program.map((piece, index) => {
