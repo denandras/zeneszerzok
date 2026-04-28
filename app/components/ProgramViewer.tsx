a1bc94ba4862cb363341537638f8e5032b7831e5
denandras <d.enandras@gmail.com>
2026-04-28 10:27:21 +0200

    Reduce footer clearance to single h-24 (96px)

--- app/components/ProgramViewer.tsx
+++ app/components/ProgramViewer.tsx
@@ -298,8 +298,7 @@ function PageContent({ piece, isAdjacent }: PageContentProps) {
         <div className="h-4 flex-shrink-0" />
 
         {/* Footer clearance - ensures all content is scrollable above fixed footer */}
-        <div className="h-32 flex-shrink-0" />
-        <div className="h-32 flex-shrink-0" />
+        <div className="h-24 flex-shrink-0" />
       </div>
     </div>
   );
